462b10b6edb207d023a59c44992575b9f0e6df33
Remo Wenger <privat@remowenger.ch>
2019-07-03 20:12:03 +0200

    Umount drives and preparation for uninstalling script from clients

--- network-share/mount.ps1
+++ network-share/mount.ps1
@@ -1,5 +1,16 @@
-$UserUPN = whoami.exe /upn
+#requires -runasadministrator
 
-net use m: \\share.flowable.group\secure /persistent:no /user:$UserUPN
-net use p: \\share.flowable.group\projects /persistent:no /user:$UserUPN
-net use s: \\share.flowable.group\software /persistent:no /user:$UserUPN
+# variables
+$install_folder_path = "$($env:systemdrive)\network-share"
+$startup_path = "$($env:programdata)\Microsoft\Windows\Start Menu\Programs\Startup\Mount Network Drives.lnk"
+$startmenu_path = "$($env:programdata)\Microsoft\Windows\Start Menu\Programs\Mount Network Drives.lnk"
+
+# umount
+net use m: /delete
+net use p: /delete
+net use s: /delete
+
+# # remove installation
+# remove-item -literalpath $install_folder_path -recurse
+# remove-item -literalpath $startup_path
+# remove-item -literalpath $startmenu_path
